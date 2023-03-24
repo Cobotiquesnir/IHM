ff254da79f0105e29d703d03a5e0699541dcf6a8
Cobotiquesnir <128813720+Cobotiquesnir@users.noreply.github.com>
2023-03-24 13:49:47 +0100

    Add files via upload

---
+++ Relier_python_html/cam.php
@@ -0,0 +1,30 @@
+<html>
+
+
+<body>
+
+	<h1>Clique sur le boutton pour lancer le programme</h1>
+	<form name="update" method="post">
+		<button name="update" type="submit"> Clique </button>
+	</form>
+	
+
+
+
+<?php
+
+	if (isset($_POST['update']))
+	{
+
+
+		echo shell_exec("python cam.py 2>&1");
+	}
+
+?>
+
+
+</body>
+
+
+
+</html>
